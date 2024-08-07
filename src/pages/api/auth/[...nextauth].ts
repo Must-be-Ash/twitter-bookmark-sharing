f89c4bd46d9cb4a66117f44130d677affa6db5fa
Must-be-Ash <arash.nouruzi@yahoo.com>
2024-08-07 14:08:32 -0700

    Your commit message here

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,42 +1,44 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "../../../lib/prisma";
 
-export default (req, res) => NextAuth(req, res, {
-  providers: [
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      return true;
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return NextAuth(req, res, {
+    providers: [
+      TwitterProvider({
+        clientId: process.env.TWITTER_CLIENT_ID,
+        clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+      async signIn({ user, account, profile }) {
+        return true;
+      },
+      async redirect({ url, baseUrl }) {
+        const username = user.name || user.username;
+        return `${baseUrl}/${username}`;
+      },
+      async session({ session, token }) {
+        if (token) {
+          session.user = token.user;
+        }
+        return session;
+      },
+      async jwt({ token, user, account, profile }) {
+        if (user) {
+          token.user = user;
+        }
+        return token;
+      },
     },
-    async redirect({ url, baseUrl }) {
-      const username = user.name || user.username;
-      return `${baseUrl}/${username}`;
+    pages: {
+      signIn: "/auth/signin",
+      signOut: "/auth/signout",
+      error: "/auth/error",
+      verifyRequest: "/auth/verify-request",
+      newUser: null,
     },
-    async session({ session, token }) {
-      if (token) {
-        session.user = token.user;
-      }
-      return session;
-    },
-    async jwt({ token, user, account, profile }) {
-      if (user) {
-        token.user = user;
-      }
-      return token;
-    },
-  },
-  pages: {
-    signIn: "/auth/signin",
-    signOut: "/auth/signout",
-    error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-    newUser: null,
-  },
-});
+  });
+}
