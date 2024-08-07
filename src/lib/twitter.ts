90c74b80ad8f49f6809f0e4a4d586b94f2e16457
Must-be-Ash <arash.nouruzi@yahoo.com>
2024-08-06 17:51:09 -0700

    Update bookmarks fetching method in twitter.ts

--- src/lib/twitter.ts
+++ src/lib/twitter.ts
@@ -4,13 +4,13 @@ const client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN!);
 
 export async function getBookmarks(userId: string) {
   try {
-    const bookmarks = await client.v2.bookmarks(userId, {
+    const bookmarks = await client.v2.bookmarks({
       'tweet.fields': ['text', 'author_id', 'created_at'],
       expansions: ['author_id'],
       'user.fields': ['name', 'username'],
     });
 
-    console.log(`Fetched ${bookmarks.data.length} bookmarks for user ${userId}`);
+    console.log(`Fetched ${bookmarks.data.length} bookmarks`);
 
     return bookmarks;
   } catch (error) {
