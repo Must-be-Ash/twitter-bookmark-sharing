5012db38f26058c88799f99933eab3e5e6dda033
Must-be-Ash <arash.nouruzi@yahoo.com>
2024-08-07 13:33:39 -0700

    Your commit message here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,20 +14,7 @@ export default function LandingPage() {
   }, []);
 
   const handleTwitterLogin = async () => {
-    try {
-      const response = await fetch('/api/auth/signin/twitter', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = await response.json();
-      if (data.url) {
-        window.location.href = data.url;
-      }
-    } catch (error) {
-      console.error('Error initiating Twitter login:', error);
-    }
+    window.location.href = '/api/auth/signin/twitter';
   };
 
   return (
@@ -36,9 +23,9 @@ export default function LandingPage() {
         <img src="/bookmark.png" alt="Bookmark" className="w-1/4 h-1/4 ml-10 mt-10" />
         <img src="/email.png" alt="Email" className="w-1/4 h-1/4 mr-10 mb-10" />
         <svg className="absolute top-0 left-0 w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path d="M10 20 L90 20" stroke="#d3d3d3" strokeWidth="1" fill="none" />
-          <path d="M10 50 L90 50" stroke="#d3d3d3" strokeWidth="1" fill="none" />
-          <path d="M10 80 L90 80" stroke="#d3d3d3" strokeWidth="1" fill="none" />
+          <path d="M0 20 Q 10 30, 20 20 T 40 20 T 60 20 T 80 20 T 100 20" stroke="#d3d3d3" strokeWidth="1" fill="none" />
+          <path d="M0 50 Q 10 60, 20 50 T 40 50 T 60 50 T 80 50 T 100 50" stroke="#d3d3d3" strokeWidth="1" fill="none" />
+          <path d="M0 80 Q 10 90, 20 80 T 40 80 T 60 80 T 80 80 T 100 80" stroke="#d3d3d3" strokeWidth="1" fill="none" />
         </svg>
       </div>
       <main className="flex-grow flex flex-col items-center justify-center p-4 z-10">
