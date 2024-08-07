4b8e62765ffbe5e3b02dbb28791ddd1a5e9d33c5
Must-be-Ash <arash.nouruzi@yahoo.com>
2024-08-07 14:03:05 -0700

    Your commit message here

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../../lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export default (req, res) => NextAuth(req, res, {
   providers: [
