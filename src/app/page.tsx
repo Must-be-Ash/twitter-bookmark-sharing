353a9c4d7f35a22bb0503c41865ebf4775877535
Must-be-Ash <arash.nouruzi@yahoo.com>
2024-08-07 22:13:17 -0700

    Your commit message here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { getServerSession } from "next-auth/next";
-import { redirect } from 'next/navigation';
 import LandingPageContent from '@/components/LandingPageContent';
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  if (session?.user?.username) {
-    redirect(`/${session.user.username}`);
-  }
-
+export default function Home() {
   return <LandingPageContent />;
 }
